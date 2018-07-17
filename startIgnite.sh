391fa5c3cb11354bcd075ae3c4f08f76c78c6158
felixanto23 <40891828+felixanto23@users.noreply.github.com>
2018-07-17 08:41:36 -0500

    Create startIgnite.sh

---
+++ startIgnite.sh
@@ -0,0 +1,19 @@
+echo "before Ignite start"
+dir=$(pwd -P)
+echo $dir
+echo $(ls -alrt)
+echo "------------------------"
+echo "0"
+echo $(ls -alrt /opt/gridgain/gridgain-enterprise-fabric-8.4.4)
+echo "0.1"
+echo "----------------------"
+filename="/opt/gridgain/gridgain-enterprise-fabric-8.4.4/bin/ignite.sh"
+echo "1"
+echo $filename
+chmod 777 $filename
+echo "2"
+sh $filename
+
+echo $(ls -alrt /opt/gridgain/gridgain-enterprise-fabric-8.4.4/bin/)
+sh $filename
+echo "After Ignite start"
