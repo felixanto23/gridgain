24cfe9b81c0fe0098720c9e47be4fcc7286333ae
felixanto23 <40891828+felixanto23@users.noreply.github.com>
2018-07-23 09:14:14 -0500

    Update startIgnite.sh

--- startIgnite.sh
+++ startIgnite.sh
@@ -4,16 +4,16 @@ echo $dir
 echo $(ls -alrt)
 echo "------------------------"
 echo "0"
-echo $(ls -alrt /opt/gridgain/gridgain-enterprise-fabric-8.4.4)
+echo $(ls -alrt /poc/gridgain/gridgain-enterprise-fabric-8.4.4)
 echo "0.1"
 echo "----------------------"
-filename="/opt/gridgain/gridgain-enterprise-fabric-8.4.4/bin/ignite.sh"
+filename="/poc/gridgain/gridgain-enterprise-fabric-8.4.4/bin/ignite.sh"
 echo "1"
 echo $filename
 chmod 777 $filename
 echo "2"
 sh $filename
 
-echo $(ls -alrt /opt/gridgain/gridgain-enterprise-fabric-8.4.4/bin/)
+echo $(ls -alrt /poc/gridgain/gridgain-enterprise-fabric-8.4.4/bin/)
 sh $filename
 echo "After Ignite start"
